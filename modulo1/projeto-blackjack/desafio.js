87d8982bbf18030ec893174c65fb307ae588cae9
Bruno Constantino <lou_carol@hotmail.com>
2022-04-04 10:08:31 -0300

    enviando desafio jogo

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -10,4 +10,16 @@
  * 
  * 
  */
+let cartaUsuario1 = comprarCarta()
+let cartaUsuario2 = comprarCarta()
+let cartaComputador1 = comprarCarta()
+let cartaComputador2 = comprarCarta()
 
+if(cartaUsuario1&&cartaUsuario2==="A"){
+   let novaCartaUsuario1 = comprarCarta()
+   let novaCartaUsuario2 = comprarCarta()
+   let cartaUsuario1 = novaCartaUsuario1
+   let cartaUsuario2 = novaCartaUsuario2
+}
+
+confirm(`Suas cartas são ${cartaUsuario1} e ${cartaUsuario2}. A carta revelada do computador é ${cartaComputador1}`)}
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -11,7 +11,7 @@
  * 
  */
 
-console.log(`Bem vindo ao jogo de Blackjack`)
+/*console.log(`Bem vindo ao jogo de Blackjack`)
 
 const carta = comprarCarta
 
@@ -25,8 +25,8 @@ if(confirm("Quer iniciar uma nova rodada?")) {
          let pontuacaoUsuario = cartaUsu1.valor + cartaUsu2.valor
          let pontuacaoMaquina = cartaMaq1.valor + cartaMaq2.valor
 
-         console.log(`Usuário - cartas: ${cartaUsu1.texto} ${cartaUsu2.texto} - ${pontuacaoUsuario}`)
-         console.log(`Maquina - cartas: ${cartaMaq1.texto} ${cartaMaq2.texto} - ${pontuacaoMaquina}`)
+         console.log(`Usuário - cartas: ${cartaUsu1.texto} ${cartaUsu2.texto} = ${pontuacaoUsuario}`)
+         console.log(`Maquina - cartas: ${cartaMaq1.texto} ${cartaMaq2.texto} = ${pontuacaoMaquina}`)
          if (pontuacaoUsuario > pontuacaoMaquina) {
             console.log("O usuário ganhou!")
             }else if (pontuacaoMaquina > pontuacaoUsuario) {
@@ -35,4 +35,28 @@ if(confirm("Quer iniciar uma nova rodada?")) {
             console.log("Empate!")   
 } else {
 	console.log("O jogo acabou")
+}*/
+/*
+console.log(`Bem vindo ao jogo de Blackjack`)
+
+if(confirm("Quer iniciar uma nova rodada?"))
+{let carta1Usuario = comprarCarta()
+let carta2Usuario = comprarCarta()
+let somaCartasUsuario = carta1Usuario.valor+carta2Usuario.valor
+
+let carta1Computador = comprarCarta()
+let carta2Computador = comprarCarta()
+let somaCartasComputador = carta1Computador.valor+carta2Computador.valor
+
+console.log(`Cartas do Usuário: ${carta1Usuario.texto} e ${carta2Usuario.texto} = pontuação ${somaCartasUsuario}
+Cartas do Computador: ${carta1Computador.texto} e ${carta2Computador.texto} = pontuação ${somaCartasComputador}`)
+
+if(somaCartasUsuario===somaCartasComputador){console.log(`Empate!`)}
+   else if(somaCartasUsuario>21){console.log(`O computador ganhou!`)}
+   else if(somaCartasUsuario>somaCartasComputador){console.log(`O usuário ganhou!`)}
+   else if(somaCartasComputador>somaCartasUsuario){console.log(`O computador ganhou!`)}
+else{console.log("O jogo acabou!")}
 }
+
+*/
+
