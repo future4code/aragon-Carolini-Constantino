9b687cd6f00a700ee6827d8cb73210b96b949c14
Bruno Constantino <lou_carol@hotmail.com>
2022-04-05 13:53:51 -0300

    enviando exercicios revisao-js

--- modulo1/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/lista-revisao-js/exercicios.js
@@ -47,7 +47,13 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  let maiorNumero = -Infitnity
+
+  for(let i=0; i<array.length; i++){
+    if(array[i]>maiorNumero){
+      maiorNumero=array[i]
+    }
+  }
 }
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
