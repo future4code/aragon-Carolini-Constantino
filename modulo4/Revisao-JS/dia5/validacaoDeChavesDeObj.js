7ae644818b40bc495b2bfe17801568952dda7669
carolini-constantino <lou_carol@hotmail.com>
2022-06-24 13:33:08 -0300

    enviando exercício dia 5

---
+++ modulo4/Revisao-JS/dia5/validacaoDeChavesDeObj.js
@@ -0,0 +1,20 @@
+function validacaoDeChavesDeObj(obj) {
+    const resultado = { isError: "", errors: [""] }
+
+    for (const prop in obj) {
+
+        if (obj[prop] === undefined) {
+            resultado.isError = true,
+                resultado.errors.push(prop)
+
+
+        } else {
+            resultado.isError = false,
+                resultado.errors = [];
+
+        }
+    }
+    return resultado;
+}
+
+console.log(validacaoDeChavesDeObj({ id: 1, name: "Carol", email: "carol@gmail.com" }));
